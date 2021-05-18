0edcf09e7f5b7bc770c0e7efb224a249fd66bda8
GAALI GAURAV KRISHNA <81814477+Gaurav-Krishna-Gaali@users.noreply.github.com>
2021-05-18 14:33:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,33 @@
-from __future__ import with_statement
-from ctypes.wintypes import MSG
-from locale import resetlocale
-from os.path import commonpath
-from re import M
+#required packages
 import telebot
 import requests
 import os
 import json, youtube_dl
 
-
+#Config vars
 token = os.environ['TELEGRAM_TOKEN']
-
-# Initialize youtube_dl
+#Intitialize YouTube downloader
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-
+#initialise  bot
 bot = telebot.TeleBot(token)
 x = bot.get_me()
 print(x)
 
+#   handling /commands  #
 
-
-@bot.message_handler(commands=['motivate'])
-def send_welc(message):
-    quote = requests.request(url='https://api.quotable.io/random', method='get')
-    bot.send_message(message.chat.id, quote.json()['content'])
-
-@bot.message_handler(commands=['start', 'help'])
+# works when /start is given
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Welcome user")
+  bot.send_message(message.chat.id, "Welcome user")
 
+# works when /motivate is given
+@bot.message_handler(commands=['motivate'])
+def send_quotes(message):
+        quote = requests.request(url='https://api.quotable.io/random',method='get')
+        bot.send_message(message.chat.id, quote.json()['content'])
+
+# works when /ytdl <link> is given
 @bot.message_handler(commands=['ytdl'])
 def down(msg):
     args = msg.text.split()[1]
@@ -37,26 +35,25 @@ def down(msg):
         with ydl:
             result = ydl.extract_info(
                 args,
-                download=False
+                download=False  # We just want to extract the info
             )
 
-            if 'entries' in result:
-                # can be a playlist or a list of videos
-                video = result['entries'][0]
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
+        else:
+            # Just a video
+            video = result
+        
+        for i in video['formats']:
+            link = '<a href=\"' + i['url'] + '\">' + 'link' + '</a>'
+
+            if i.get('format_note'):
+                bot.reply_to(msg, 'Quality- ' + i['format_note'] + ': ' + link, parse_mode='HTML')
             else:
-                # Just a video
-                video = result
-
-            for i in video['formats']:
-                link = '<a href=\"'+ i['url'] + '\">' + 'link' + '</a>'
-
-                if i.get('format_note'):
-                    bot.reply_to(msg, 'Quality- ' + i['format_note'] + ':' + link, parse_mode='HTML')
-
-                else:
-                    bot.reply_to(msg, link, parse_mode="HTML", display_notification=True)
-
+                bot.reply_to(msg, link, parse_mode='HTML', disable_notification=True)
     except:
         bot.reply_to(msg, 'This can\'t be downloaded by me')
 
+#pool~start the bot
 bot.polling()
